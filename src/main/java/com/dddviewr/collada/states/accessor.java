113ecb43a375ad8c6969d4cf944ec6b5d586ce84
sonisourabh <59862118+sonisourabh@users.noreply.github.com>
2021-05-16 00:30:16 +0530

    Use spotless-maven-plugin for code formating

--- src/main/java/com/dddviewr/collada/states/accessor.java
+++ src/main/java/com/dddviewr/collada/states/accessor.java
@@ -8,18 +8,20 @@ import com.dddviewr.collada.State;
 import com.dddviewr.collada.StateManager;
 
 public class accessor extends State {
-	protected Accessor theAccessor;
+  protected Accessor theAccessor;
 
-	public void init(String name, Attributes attrs, StateManager mngr) {
-		super.init(name, attrs, mngr);
-		this.theAccessor = new Accessor(attrs.getValue("source"), Integer
-				.parseInt(attrs.getValue("count")), Integer.parseInt(attrs
-				.getValue("stride")));
-		Source source = ((source) getParent().getParent()).getSource();
-		source.setAccessor(this.theAccessor);
-	}
+  public void init(String name, Attributes attrs, StateManager mngr) {
+    super.init(name, attrs, mngr);
+    this.theAccessor =
+        new Accessor(
+            attrs.getValue("source"),
+            Integer.parseInt(attrs.getValue("count")),
+            Integer.parseInt(attrs.getValue("stride")));
+    Source source = ((source) getParent().getParent()).getSource();
+    source.setAccessor(this.theAccessor);
+  }
 
-	public Accessor getAccessor() {
-		return this.theAccessor;
-	}
+  public Accessor getAccessor() {
+    return this.theAccessor;
+  }
 }
